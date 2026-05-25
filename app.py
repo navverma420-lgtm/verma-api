8540a6ba8e4f8124f131a0dee55a1f10893a6fca
navverma420-lgtm <navverma420@gmail.com>
2026-05-25 12:52:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,52 +1,27 @@
 from flask import Flask, request, jsonify
 import google.generativeai as genai
+import os
 
 app = Flask(__name__)
 
-genai.configure(
-    api_key="AIzaSyD3a-9AfrSFXpPHYFQBkT3xcxYLKIjxWP8"
-)
-
-model = genai.GenerativeModel(
-    "gemini-1.5-flash-latest"
-)
+genai.configure(api_key=os.environ.get("GEMINI_API_KEY"))
+model = genai.GenerativeModel("gemini-2.0-flash")
 
 @app.route("/")
 def home():
-
     return "NAV AI ONLINE"
-
 
 @app.route("/ai/ask", methods=["GET"])
 def ask_ai():
-
     question = request.args.get("question")
-
     if not question:
-        return jsonify({
-            "reply": "Question missing"
-        })
-
+        return jsonify({"reply": "Question missing"})
     try:
-
-        response = model.generate_content(
-            question
-        )
-
+        response = model.generate_content(question)
         answer = response.text
-
     except Exception as e:
-
         answer = str(e)
-
-    return jsonify({
-        "reply": answer
-    })
-
+    return jsonify({"reply": answer})
 
 if __name__ == "__main__":
-
-    app.run(
-        host="0.0.0.0",
-        port=5000
-    )
+    app.run(host="0.0.0.0", port=5000)
